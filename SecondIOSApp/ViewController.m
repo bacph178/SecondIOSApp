6957d5ea67d766e307afb0353dd6683045ebafcd
Phung Hoang Bac <phung.hoang.bac@framgia.com>
2014-09-16 13:41:46 +0700

    Presenting Sharing Options with UIActivityViewController

--- SecondIOSApp/ViewController.m
+++ SecondIOSApp/ViewController.m
@@ -8,16 +8,67 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITextFieldDelegate>
+
+@property (nonatomic, strong) UITextField *textField;
+@property (nonatomic, strong) UIButton *buttonShare;
+@property (nonatomic, strong) UIActivityViewController *activityViewController;
 
 @end
 
 @implementation ViewController
 
+- (void)createTextField {
+    self.textField = [[UITextField alloc] initWithFrame:CGRectMake(20.0f,
+                                                                   35.0f,
+                                                                   280.0f,
+                                                                   30.0f)];
+    self.textField.translatesAutoresizingMaskIntoConstraints = NO;
+    self.textField.borderStyle = UITextBorderStyleRoundedRect;
+    self.textField.placeholder = @"Enter text to share...";
+    self.textField.delegate = self;
+    [self.view addSubview:self.textField];
+}
+
+- (void)createButton {
+    self.buttonShare = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    self.buttonShare.translatesAutoresizingMaskIntoConstraints = NO;
+    self.buttonShare.frame = CGRectMake(20.0f, 80.0f, 280.0f, 44.0f);
+    [self.buttonShare setTitle:@"Share" forState:UIControlStateNormal];
+    [self.buttonShare addTarget:self
+                         action:@selector(handleShare:)
+               forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:self.buttonShare];
+}
+
+- (void)handleShare:(id)paramSender {
+    if ([self.textField.text length] == 0) {
+        NSString *message = @"Please enter a text and then press Share";
+        UIAlertView *alerView = [[UIAlertView alloc] initWithTitle:nil
+                                                           message:message
+                                                          delegate:nil
+                                                 cancelButtonTitle:@"OK"
+                                                 otherButtonTitles:nil];
+        [alerView show];
+        return;
+    }
+    self.activityViewController = [[UIActivityViewController alloc]
+                                   initWithActivityItems:@[self.textField.text]
+                                   applicationActivities:nil];
+    [self presentViewController:self.activityViewController
+                       animated:YES completion:^{}];
+}
+
+- (BOOL) textFieldShouldReturn:(UITextField *)textField {
+    [textField resignFirstResponder];
+    return YES;
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    [self createTextField];
+    [self createButton];
 }
 
 - (void)didReceiveMemoryWarning
